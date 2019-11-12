ba27e859e40770641a2fc0ed6eeec481769d0b4b
t-ataku <31171720+t-ataku@users.noreply.github.com>
2019-11-12 16:14:28 +0900

    Update hello-world.js

--- hello-world.js
+++ hello-world.js
@@ -6,7 +6,7 @@ const port = 8080;
 const server = http.createServer((req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/plain');
-	res.end('Hello, World\n');
+	res.end('Hello, Node.js World\n');
     });
 
 server.listen(port, hostname, () => {
