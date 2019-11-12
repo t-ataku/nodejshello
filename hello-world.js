29d16dec1803cd4f3d617af6644fca530b62aafb
Toshimi Ataku <ATAKU@jp.ibm.com>
2019-11-12 16:47:57 +0900

    Fix variable name

--- hello-world.js
+++ hello-world.js
@@ -6,11 +6,11 @@ const port = 8080;
 const server = http.createServer();
 
 server.listen(port, hostname, () => {
-	console.log(`Server running at http://${hostname}:${port}/`);
-    });
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
 
 server.on('request', (req, res) => {
-    const { method, url } = request;
+    const { method, url } = req;
     res.statusCode = 200;
     res.setHeader('content-type', 'text/plain');
     res.end(`METHOD: ${method}\nURI: ${url}\n`);
