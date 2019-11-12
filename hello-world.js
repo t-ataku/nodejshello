ccb78e12fcae857c6a590127c7fd99825b0f514d
Toshimi Ataku <ATAKU@jp.ibm.com>
2019-11-12 16:55:11 +0900

    Change response type to "text/html" and add form for post test

--- hello-world.js
+++ hello-world.js
@@ -12,6 +12,13 @@ server.listen(port, hostname, () => {
 server.on('request', (req, res) => {
     const { method, url } = req;
     res.statusCode = 200;
-    res.setHeader('content-type', 'text/plain');
-    res.end(`METHOD: ${method}\nURI: ${url}\n`);
+    res.setHeader('content-type', 'text/html');
+    res.end(`<html><head><title>post form</title></head><body>METHOD: ${method}<br>URI: ${url}<br><form method="POST"><input type=text value="Input something here"><br><input type=submit></form></body></html>`);
+});
+
+let body = [];
+request.on('data', (chunk) => {
+    body.push(chunk);
+}).on('end', () => {
+    body = Buffer.concat(body).toString();
 });
